3284821023d6a3515799848e8dfd5d2c419444d3
janhandl <jh@alphainside.eu>
2023-05-07 04:39:17 +0200

    added color theming

---
+++ src/config/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    main : '#012038',
+    second : '#1D6994',
+    white : '#FFFFFF',
+    black : '#000000',
+    grey : '#B8AFAF'
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,32 @@
 import '@/styles/globals.css'
+import {createTheme, ThemeProvider} from "@mui/material";
+import {colors} from "@/config/colors";
+const theme = createTheme({
+  palette: {
+    typography: {
+      fontFamily: [
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+    },
+    primary: {
+      main: colors.main,
+    },
+    secondary: {
+      main: colors.second,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+  );
 }
