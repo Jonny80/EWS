496ee33ed0458d3aeffbe3232eecb4e6ae498dc9
janhandl <jh@alphainside.eu>
2023-05-07 05:15:36 +0200

    fided title and button styles

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link rel="icon" href="/logo.svg" />
+          <title>Multi Cloud</title>
+          <meta name="theme-color" content="#012038" />
+          <meta name="background-color" content="#012038" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import styles from '@/styles/Home.module.css'
 import {Button} from "@mui/material";
+import {colors} from "@/config/colors";
 export default function Home() {
   return (
-    <div style={{display:'flex',height:'100vh',justifyContent:'center',alignItems:'center'}}>
-        <Button variant={'outlined'} style={{borderRadius:'0.3rem'}}>Install</Button>
+    <div style={{display:'flex',height:'100vh',width:'100vw',justifyContent:'center',alignItems:'center',backgroundColor:colors.main}}>
+        <Button variant={'outlined'} style={{borderRadius:'0.3rem',backgroundColor:colors.white}}>Install</Button>
     </div>
   )
 }
