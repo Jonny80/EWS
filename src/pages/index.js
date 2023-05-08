8f9c3808744fe4993142be5abe901e8c2f89ad3f
janhaendl <jh@alphainside.eu>
2023-05-08 03:29:47 +0200

    added router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import {Button} from "@mui/material";
 import {colors} from "@/config/colors";
+import { useRouter } from 'next/router';
+
 export default function Home() {
+
+    const router = useRouter()
+
   return (
     <div style={{display:'flex',height:'100vh',width:'100vw',justifyContent:'center',alignItems:'center',backgroundColor:colors.main}}>
-        <Button variant={'outlined'} style={{borderRadius:'0.3rem',backgroundColor:colors.white}}>Install</Button>
+        <Button onClick={()=>router.push('/main')} variant={'outlined'} style={{borderRadius:'0.3rem',backgroundColor:colors.white}}>Install</Button>
     </div>
   )
 }
